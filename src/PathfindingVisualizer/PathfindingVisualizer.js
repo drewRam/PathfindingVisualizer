4ef4226d257ecf256ef9ebe32589bea9b92535f4
Andrew Ramirez <andrewramirez@unomaha.edu>
2022-08-24 17:35:52 -0700

    first commit

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Node from './Node/Node';
+import './PathfindingVisualizer.css';
+
+const PathfindingVisualizer = () => {
+    return(
+        <div>
+            <Node/>
+        </div>
+    );
+}
+
+export default PathfindingVisualizer;
